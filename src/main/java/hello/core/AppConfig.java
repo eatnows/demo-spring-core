eea9a48420cc1983a54fae1d996e0bf34e91d1ef
eatnows <gagyasindev@gmail.com>
2022-01-13 22:34:49 +0900

    feat: configuration과 CGLIB

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -12,23 +11,32 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// @Bean만 있어도 스프링 빈으로 등록되지만 싱글톤을 보장해주지 않는다. 순수 자바코드가 실행
+// @Configuration이 있어야 CGLIB이 실행되어 싱글톤으로 스프링 빈을 등록해준다.
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
     @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
